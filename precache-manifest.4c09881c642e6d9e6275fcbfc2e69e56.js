f93f8307454d8323ca7a4c530e020f671a879e1e
Peter Linder <palinder@umich.edu>
2020-06-06 21:48:35 -0400

    Updates

---
+++ precache-manifest.4c09881c642e6d9e6275fcbfc2e69e56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5861be74a0eda105eb12eebef8a458e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0685518e2c4ea66f5f80",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "6c2b4ee56bab9e64ec90",
+    "url": "/static/css/main.7340fc1c.chunk.css"
+  },
+  {
+    "revision": "0685518e2c4ea66f5f80",
+    "url": "/static/js/2.adc11136.chunk.js"
+  },
+  {
+    "revision": "9192044114dfd3ef1f16a8bb55426941",
+    "url": "/static/js/2.adc11136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c2b4ee56bab9e64ec90",
+    "url": "/static/js/main.4fab18aa.chunk.js"
+  },
+  {
+    "revision": "f8f36e386adae7e69567",
+    "url": "/static/js/runtime-main.24c30e29.js"
+  }
+]);
