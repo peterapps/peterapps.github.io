e4fc3108afd5b3bd3201ee93f02f43850a3db1e8
Peter Linder <palinder@umich.edu>
2020-06-08 12:35:27 -0400

    Updates

---
+++ precache-manifest.d5ccbda60494b40c5b98787046f67dd6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726c890c0dee119e33712459e971d402",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7da49c69401b3b30d270",
+    "url": "/static/css/13.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "f1872310180381899918",
+    "url": "/static/css/main.36f4974f.chunk.css"
+  },
+  {
+    "revision": "7da49c69401b3b30d270",
+    "url": "/static/js/13.347aa903.chunk.js"
+  },
+  {
+    "revision": "9192044114dfd3ef1f16a8bb55426941",
+    "url": "/static/js/13.347aa903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9ce4897ea9851ef1957",
+    "url": "/static/js/14.c45095ce.chunk.js"
+  },
+  {
+    "revision": "f1872310180381899918",
+    "url": "/static/js/main.d634bbe2.chunk.js"
+  },
+  {
+    "revision": "be23eea12f3f052d3141",
+    "url": "/static/js/reactPlayerDailyMotion.9d093f4d.chunk.js"
+  },
+  {
+    "revision": "a9110c3b3fa0dfef2fc4",
+    "url": "/static/js/reactPlayerFacebook.42a1c222.chunk.js"
+  },
+  {
+    "revision": "4ccdb5a06c59b1115e16",
+    "url": "/static/js/reactPlayerFilePlayer.909341ad.chunk.js"
+  },
+  {
+    "revision": "596e303d3d0e5db55027",
+    "url": "/static/js/reactPlayerMixcloud.59da04f5.chunk.js"
+  },
+  {
+    "revision": "a959926dc0d4c5cd9455",
+    "url": "/static/js/reactPlayerSoundCloud.4f0321a6.chunk.js"
+  },
+  {
+    "revision": "dd6a8086af9cfe73ad7d",
+    "url": "/static/js/reactPlayerStreamable.94f06dde.chunk.js"
+  },
+  {
+    "revision": "f30e62df657d93eef4c0",
+    "url": "/static/js/reactPlayerTwitch.d4fd8d1b.chunk.js"
+  },
+  {
+    "revision": "03795f2fffecaaec3210",
+    "url": "/static/js/reactPlayerVidyard.6b4f976f.chunk.js"
+  },
+  {
+    "revision": "9050d11e6390a8996566",
+    "url": "/static/js/reactPlayerVimeo.4287464b.chunk.js"
+  },
+  {
+    "revision": "a7289528c45c0cb1b97c",
+    "url": "/static/js/reactPlayerWistia.bbe287fd.chunk.js"
+  },
+  {
+    "revision": "be0950ebc05a5bce45e3",
+    "url": "/static/js/reactPlayerYouTube.88340f93.chunk.js"
+  },
+  {
+    "revision": "53b1a5d170a91ea32755",
+    "url": "/static/js/runtime-main.76c0c70b.js"
+  }
+]);
